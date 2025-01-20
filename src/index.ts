b5b4a6c21e4d85816f4d7b8a036c1ad838573147
gcnew <marinov.ms@gmail.com>
2025-01-20 15:26:14 +0200

    Improve shortcut support

--- src/index.ts
+++ src/index.ts
@@ -6,8 +6,10 @@ let height: number;
 
 let debug: boolean = true;
 
+const WorldZoom = 20;
+
 let editorMode = false;
-let gridSize = 20;
+let gridSize = WorldZoom;
 
 const frameWindow = 1000;
 let   frameRateBuffer: number[] = [];
@@ -16,15 +18,111 @@ let lastT: number = 0;
 
 let gameStop = false;
 
-const pressedKeys: { [key: string]: string } = {};
-
-window.onload = setup;
+const pressedKeys: { [key in typeof KeyMap[keyof typeof KeyMap]]?: true | undefined } = {};
 
 let mouseX: number;
 let mouseY: number;
 
 let clickX: number | undefined;
 let clickY: number | undefined;
+
+const isMac = /Mac/.test(navigator.platform);
+
+const KeyMap = {
+    F1:           'F1',
+    F2:           'F2',
+    F3:           'F3',
+    F4:           'F4',
+    F5:           'F5',
+    F6:           'F6',
+    F7:           'F7',
+    F8:           'F8',
+    F9:           'F9',
+    F10:          'F10',
+    F11:          'F11',
+    F12:          'F12',
+
+    Digit1:       '1',
+    Digit2:       '2',
+    Digit3:       '3',
+    Digit4:       '4',
+    Digit5:       '5',
+    Digit6:       '6',
+    Digit7:       '7',
+    Digit8:       '8',
+    Digit9:       '9',
+    Digit0:       '0',
+
+    KeyA:         'A',
+    KeyB:         'B',
+    KeyC:         'C',
+    KeyD:         'D',
+    KeyE:         'E',
+    KeyF:         'F',
+    KeyG:         'G',
+    KeyH:         'H',
+    KeyI:         'I',
+    KeyJ:         'J',
+    KeyK:         'K',
+    KeyL:         'L',
+    KeyM:         'M',
+    KeyN:         'N',
+    KeyO:         'O',
+    KeyP:         'P',
+    KeyQ:         'Q',
+    KeyR:         'R',
+    KeyS:         'S',
+    KeyT:         'T',
+    KeyU:         'U',
+    KeyV:         'V',
+    KeyW:         'W',
+    KeyX:         'X',
+    KeyY:         'Y',
+    KeyZ:         'Z',
+
+    ShiftLeft:    'SHIFT',
+    ShiftRight:   'SHIFT',
+    ControlLeft:  'CTRL',
+    ControlRight: 'CTRL',
+    AltLeft:      'ALT',
+    AltRight:     'ALT',
+    MetaLeft:     'META',
+    MetaRight:    'META',
+
+    Escape:       'ESC',
+    Tab:          'TAB',
+    Backspace:    'BACKSPACE',
+    Delete:       'DELETE',
+    Enter:        'ENTER',
+    CapsLock:     'CAPSLOCK',
+    Space:        'SPACE',
+
+    ArrowLeft:    'LEFT',
+    ArrowRight:   'RIGHT',
+    ArrowUp:      'UP',
+    ArrowDown:    'DOWN',
+
+    Minus:        '-',
+    Equal:        '=',
+    BracketLeft:  '[',
+    BracketRight: ']',
+    Semicolon:    ';',
+    Quote:        '\'',
+    Backslash:    '\\',
+    Backquote:    '`',
+    Comma:        ',',
+    Period:       '.',
+    Slash:        '/',
+} as const;
+
+let KbShortcuts = parseShortcuts(new Map([
+    [toggleEditor, isMac ? 'META + E' : 'CTRL + E'],
+    [resetZoom,    isMac ? 'META + 0' : 'CTRL + 0'],
+    [toggleDebug,  'D'],
+    [toggleRun,    'S'],
+] as const));
+
+window.onload = setup;
 
 
 function setup() {
@@ -46,19 +144,22 @@ function setup() {
     window.addEventListener('keyup',   keyupListener);
 
     window.addEventListener('keydown', e => {
-        if (e.code === 'KeyD' && !e.repeat) {
-            debug = !debug;
+        // skip repeats
+        if (e.repeat) {
+            return;
         }
 
-        if (e.code === 'KeyS' && !e.repeat) {
-            gameStop = !gameStop;
-            !gameStop && window.requestAnimationFrame(draw);
-        }
+        const sigil = [
+                e.metaKey  && 'META',
+                e.ctrlKey  && 'CTRL',
+                e.altKey   && 'ALT',
+                e.shiftKey && 'SHIFT',
+                KeyMap[e.code as keyof typeof KeyMap] || e.code
+            ]
+            .filter(isTruthy)
+            .join('+');
 
-        if (e.code === 'KeyE' && !e.repeat) {
-            editorMode = !editorMode;
-            toggleEditor();
-        }
+        KbShortcuts.get(sigil)?.();
     });
 
     canvas.addEventListener('wheel', e => {
@@ -82,6 +183,43 @@ function setup() {
     });
 }
 
+function toggleDebug() {
+    debug = !debug;
+}
+
+function toggleRun() {
+    gameStop = !gameStop;
+    !gameStop && window.requestAnimationFrame(draw);
+}
+
+function resetZoom() {
+    gridSize = WorldZoom;
+}
+
+type Falsy = false | 0 | '' | null | undefined;
+
+// this is a type predicate - if x is `truthy`, then it's T
+const isTruthy = <T>(x: T | Falsy): x is T => !!x;
+
+function parseShortcuts(m: Map<() => void, string>): Map<string, () => void> {
+    const prio = [ 'META', 'CTRL', 'ALT', 'SHIFT' ];
+
+    const entries = [... m.entries()].map(([handler, shortcut]) => {
+        const fixed = shortcut
+            .toUpperCase()
+            .split(/\s*\+\s*/g)
+            .sort(x => {
+                const idx = prio.indexOf(x);
+                return idx !== -1 ? idx : x.charCodeAt(0);
+            })
+            .join('+');
+
+        return [fixed, handler] as const;
+    });
+
+    return new Map(entries);
+}
+
 function clamp(x: number, min: number, max: number) {
     return Math.max(min, Math.min(x, max));
 }
@@ -100,24 +238,36 @@ function resize() {
 }
 
 function keydownListener(e: KeyboardEvent) {
-    const code = e.altKey   ? 'ALT'   :
-                 e.metaKey  ? 'META'  :
-                 e.shiftKey ? 'SHIFT' :
-                 e.code;
+    const code = KeyMap[e.code as keyof typeof KeyMap] || e.code;
 
-    pressedKeys[code] = code;
+    // tab has a special meaning; if it is fired point-blank, we cancel the default behaviour
+    // otherwise, the keyup event might never be received if the window loses focus
+    if (e.code === 'Tab' && e.target === document.body) {
+        e.preventDefault();
+    }
+
+    pressedKeys[code] = true;
 }
 
 function keyupListener(e: KeyboardEvent) {
-    const code = e.altKey   ? 'ALT'   :
-                 e.metaKey  ? 'META'  :
-                 e.shiftKey ? 'SHIFT' :
-                 e.code;
+    const code = KeyMap[e.code as keyof typeof KeyMap] || e.code;
+
+    // when holding the meta (command) key, key-up events are not fired sans CapsLock
+    // so we need to clear all pressed keys here
+    if (isMac && code === 'META') {
+        const keys = Object.keys(pressedKeys) as (keyof typeof pressedKeys)[];
+        for (const k of keys) {
+            if (k !== 'CAPSLOCK') {
+                delete pressedKeys[k];
+            }
+        }
+    }
 
     delete pressedKeys[code];
 }
 
 function toggleEditor() {
+    editorMode = !editorMode;
     document.getElementById('sidebar')!.style.display = editorMode ? 'initial' : 'none';
     resize();
 }
@@ -133,13 +283,22 @@ function drawDebug() {
         min = frameRateBuffer.reduce((x, y) => x < y ? x : y);
     }
 
-    ctx.fillText(`w:${width} h:${height} fps:${fps}`, width - 130, 10);
-    ctx.fillText(`min:${min?.toFixed(2)} max:${max?.toFixed(2)}`, width - 130, 20);
+    const keys = Object.keys(pressedKeys);
 
-    ctx.fillText(`zoom:${gridSize}`, width - 130, 30);
-    ctx.fillText(`x:${mouseX} y:${mouseY}`, width - 130, 40);
+    const msg = [
+        `w:${width} h:${height} fps:${fps}`,
+        `min:${min?.toFixed(2)} max:${max?.toFixed(2)}`,
+        `zoom:${gridSize}`,
+        `x:${mouseX} y:${mouseY}`,
 
-    clickX && ctx.fillText(`x:${clickX} y:${clickY}`, width - 130, 50);
+        clickX && `cx:${clickX} cy:${clickY}`,
+
+        keys.length && `keys:${keys.join(', ')}`
+    ].filter(isTruthy);
+
+    for (let i = 0; i < msg.length; ++i) {
+        ctx.fillText(msg[i], width - 130, (i + 1) * 10);
+    }
 
     // const dims = ctx.measureText(msg);
     // ctx.fillText(msg, width - 10 - dims.width, height - dims.fontBoundingBoxAscent);
